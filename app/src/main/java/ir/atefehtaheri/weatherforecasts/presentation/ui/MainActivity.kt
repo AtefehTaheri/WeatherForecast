cbc496c65b52bb72de1074214e0d045018466819
AtefehTaheri <e.atefehtaheri@gmail.com>
2023-11-18 15:39:36 +0330

    initial

---
+++ app/src/main/java/ir/atefehtaheri/weatherforecasts/presentation/ui/MainActivity.kt
@@ -0,0 +1,48 @@
+package ir.atefehtaheri.weatherforecasts.presentation.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ir.atefehtaheri.weatherforecasts.BuildConfig
+import ir.atefehtaheri.weatherforecasts.presentation.ui.theme.WeatherForecastsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherForecastsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting(BuildConfig.API_KEY)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WeatherForecastsTheme {
+        Greeting("Android")
+
+    }
+}
